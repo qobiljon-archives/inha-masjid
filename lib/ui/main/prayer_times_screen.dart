6b364b937e77626c623f4077fef0e16200ba720a
otabekinha <otabekinha@gmail.com>
2024-01-11 13:00:29 +0900

    Added new clock to the prayer times screen

--- lib/ui/main/prayer_times_screen.dart
+++ lib/ui/main/prayer_times_screen.dart
@@ -8,6 +8,7 @@ import 'package:flutter/material.dart';
 // 3rd party
 import 'package:google_fonts/google_fonts.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:analog_clock/analog_clock.dart';
 
 // Local
 import 'package:inha_masjid/utils/colors.dart';
@@ -83,98 +84,88 @@ class _PrayerTimesScreenState extends State<PrayerTimesScreen> {
         ),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          // Current time
-          Container(
-            width: double.infinity,
-            margin: const EdgeInsets.symmetric(horizontal: 40),
-            padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 5),
-            decoration: BoxDecoration(
-              color: AppColors.cardPrimaryButtonColor,
-              borderRadius: BorderRadius.circular(50),
-            ),
-            child: Center(
-              child: Text(
-                timeStr,
-                style: GoogleFonts.manrope(
-                  fontSize: 35,
-                  color: AppColors.white,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: AnalogClock(),
             ),
           ),
-
           // Prayer times
-          Center(
-            child: Wrap(
-              spacing: 22, // Adjust the spacing between prayer times
-              runSpacing: 40, // Adjust the spacing between lines
-              children: [
-                // Each individual prayer time
-                for (var prayerName in AppStrings.prayerNames)
-                  Column(
-                    children: [
-                      Text(
-                        prayerName.capitalize(),
-                        style: GoogleFonts.manrope(
-                          fontSize: AppDimensions.prayerTimesTextFontSize,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      const SizedBox(height: 5),
-                      Container(
-                        width: 140,
-                        height: 58,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(16),
-                          border: Border.all(
-                            color: AppColors.textSecondary,
-                            width: 2.0,
+          Expanded(
+            child: Center(
+              child: Wrap(
+                spacing: 22, // Adjust the spacing between prayer times
+                runSpacing: 10, // Adjust the spacing between lines
+                children: [
+                  // Each individual prayer time
+                  for (var prayerName in AppStrings.prayerNames)
+                    Column(
+                      children: [
+                        Text(
+                          prayerName.capitalize(),
+                          style: GoogleFonts.manrope(
+                            fontSize: AppDimensions.prayerTimesTextFontSize,
+                            fontWeight: FontWeight.bold,
                           ),
                         ),
-                        child: StreamBuilder<DocumentSnapshot>(
-                          stream: FirebaseFirestore.instance
-                              .doc(FirestorePaths.prayerTimeDoc(prayerName))
-                              .snapshots(),
-                          builder: (context, snapshot) {
-                            if (!snapshot.hasData) {
-                              return const Center(
-                                child: CircularProgressIndicator(),
-                              );
-                            }
+                        const SizedBox(height: 5),
+                        Container(
+                          width: 140,
+                          height: 58,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(16),
+                            border: Border.all(
+                              color: AppColors.textSecondary,
+                              width: 2.0,
+                            ),
+                          ),
+                          child: StreamBuilder<DocumentSnapshot>(
+                            stream: FirebaseFirestore.instance
+                                .doc(FirestorePaths.prayerTimeDoc(prayerName))
+                                .snapshots(),
+                            builder: (context, snapshot) {
+                              if (!snapshot.hasData) {
+                                return const Center(
+                                  child: CircularProgressIndicator(),
+                                );
+                              }
 
-                            // Prayer time details
-                            var doc = snapshot.data!;
-                            int hour = doc['hour'];
-                            int minute = doc['minute'];
+                              // Prayer time details
+                              var doc = snapshot.data!;
+                              int hour = doc['hour'];
+                              int minute = doc['minute'];
 
-                            // Highlight if within next 1 hour
-                            int deltaMinutes = (hour - now.hour) * 60 + (minute - now.minute);
-                            bool nextPrayer = deltaMinutes >= 0 && deltaMinutes <= 60;
+                              // Highlight if within next 1 hour
+                              int deltaMinutes = (hour - now.hour) * 60 +
+                                  (minute - now.minute);
+                              bool nextPrayer =
+                                  deltaMinutes >= 0 && deltaMinutes <= 60;
 
-                            // Return the prayer time widget
-                            return Center(
-                              child: Text(
-                                '${hour.toString().padLeft(2, '0')}:${minute.toString().padLeft(2, '0')}',
-                                style: GoogleFonts.manrope(
-                                  textStyle: TextStyle(
-                                    fontSize: AppDimensions.prayerTimesFontSize,
-                                    fontWeight: FontWeight.bold,
-                                    color: nextPrayer
-                                        ? AppColors.cardPrimaryButtonColor
-                                        : AppColors.textPrimary,
+                              // Return the prayer time widget
+                              return Center(
+                                child: Text(
+                                  '${hour.toString().padLeft(2, '0')}:${minute.toString().padLeft(2, '0')}',
+                                  style: GoogleFonts.manrope(
+                                    textStyle: TextStyle(
+                                      fontSize:
+                                          AppDimensions.prayerTimesFontSize,
+                                      fontWeight: FontWeight.bold,
+                                      color: nextPrayer
+                                          ? AppColors.cardPrimaryButtonColor
+                                          : AppColors.textPrimary,
+                                    ),
                                   ),
                                 ),
-                              ),
-                            );
-                          },
+                              );
+                            },
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-              ],
+                      ],
+                    ),
+                ],
+              ),
             ),
           ),
         ],
